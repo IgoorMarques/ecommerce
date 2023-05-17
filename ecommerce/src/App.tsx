3a3fe5ac9890f3aa0e5ab6119a56bc87f9c9f3c3
igor <igo12345marques@gmail.com>
2023-05-17 15:36:52 -0300

    inserindo o eslint no projeto

---
+++ ecommerce/src/App.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text} from "react-native";
+
+const App = ({ x }:any) =>{
+    return(
+        <SafeAreaView>
+            <Text>Testando</Text>
+        </SafeAreaView>
+    )
+};
+
+export default App;
